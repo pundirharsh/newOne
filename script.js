ef882868980c5aebf470cf115192fad88b658966
pundirharsh <157054991+pundirharsh@users.noreply.github.com>
2024-09-30 13:05:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for toggling the menu in mobile view
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.menu ul').classList.toggle('show');
+  });
+  
